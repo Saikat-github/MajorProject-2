be19ec188d94c0b136cb527079cdd974650aea20
Saikat Saha <saikatsaha.rph@gmail.com>
2024-04-15 14:07:33 +0530

    changes made to add corousel

--- public/js/map.js
+++ public/js/map.js
@@ -1,13 +1,14 @@
 mapboxgl.accessToken = mapToken;
-
 const map = new mapboxgl.Map({
-  container: 'map', // container ID
-  style: "mapbox://styles/mapbox/streets-v12",
-  center: coordinates, // starting position [lng, lat]
-  zoom: 9, // starting zoom
-  });
+    container: 'map', // container ID
+    // style: "mapbox://styles/mapbox/street-v12",
+    center: coordinates, // starting position [lng, lat]
+    zoom: 9 // starting zoom
+});
+
 
 const marker = new mapboxgl.Marker({color: "red"})
-  .setLngLat(coordinates) //Listing.geometry.coordinates
-  .setPopup(new mapboxgl.Popup({offset: 25}).setHTML(`<h4>${listing.title}</h4><p>Exact location provided after booking</p>`))
-  .addTo(map);
+   .setLngLat(coordinates) //Listing.geometry.coordinates
+   .setPopup(new mapboxgl.Popup({offset: 25})
+   .setHTML("<p>Exact location provided after booking</p>"))
+   .addTo(map);
