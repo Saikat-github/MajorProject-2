11e62d31da4e9af98c6e133d0fa35d9fb7c3f995
Saikat Saha <saikatsaha.rph@gmail.com>
2024-04-18 17:38:34 +0530

    modified contact details

--- schema.js
+++ schema.js
@@ -8,7 +8,7 @@ module.exports.listingSchema = Joi.object({
         country: Joi.string().required(),
         price: Joi.number().required().min(0),
         image: Joi.string().allow("", null),
-        contact: Joi.number().required()
+        contact: Joi.number().min(100000000).max(9999999999).required()
     }).required()
 });
 
